d7a0b63105c5b6890bc7214aa53f68a03e232ae8
Csaba Kiraly <kiraly@disi.unitn.it>
2011-03-30 10:24:51 +0200

    move things to THIRDPARTYLIBS and add some error handling

--- peerstreamer-download-build-test.sh
+++ peerstreamer-download-build-test.sh
@@ -4,34 +4,41 @@ mkdir PeerStreamer
 cd PeerStreamer
 
 BASEDIR=`pwd`
+THIRDPARTYLIBS=$BASEDIR/THIRDPARTY-LIBS
+
+mkdir $THIRDPARTYLIBS
+cd $THIRDPARTYLIBS
 
 #prepare x264 (optional)
 git clone git://git.videolan.org/x264.git
 cd x264
-./configure --prefix=$BASEDIR/x264-install/
-make -j 2
-make install
+./configure --prefix=$THIRDPARTYLIBS/x264-install/ || { echo "Error configuring x264" && exit 1; }
+make -j 2 || { echo "Error compiling x264" && exit 1; }
+make install  || { echo "Error installing x264" && exit 1; }
 cd ..
 
 #prepare ffmpeg
 git clone git://git.videolan.org/ffmpeg.git
 cd ffmpeg
-./configure --enable-libx264 --enable-gpl --enable-pthreads --extra-cflags=-I$BASEDIR/x264-install/include --extra-ldflags=-L$BASEDIR/x264-install/lib --prefix=$BASEDIR/ffmpeg-install
+./configure --enable-libx264 --enable-gpl --enable-pthreads --extra-cflags=-I$THIRDPARTYLIBS/x264-install/include --extra-ldflags=-L$THIRDPARTYLIBS/x264-install/lib --prefix=$THIRDPARTYLIBS/ffmpeg-install || { echo "Error configuring ffmpeg" && exit 1; }
 #in case x264 is not reqired (do we need the encoding?): ./configure --enable-gpl --enable-pthreads --prefix=$BASEDIR/ffmpeg-install
-make -j 2
-make install
+make -j 2 || { echo "Error cimpiling ffmpeg" && exit 1; }
+make install || { echo "Error installing ffmpeg" && exit 1; }
 cd ..
+
+#cd .. #3RDPARTY-LIBS
 
 #prepare GRAPES
 git clone http://halo.disi.unitn.it/~abeni/PublicGits/GRAPES.git
 cd GRAPES
-FFDIR=$BASEDIR/ffmpeg make
-cd ..
+FFDIR=$THIRDPARTYLIBS/ffmpeg make || { echo "Error cimpiling GRAPES" && exit 1; }
+cd ../..
 
 #prepare the Streamer
 git clone http://halo.disi.unitn.it/~cskiraly/PublicGits/Streamers.git
 cd Streamers
-GRAPES=$BASEDIR/GRAPES FFMPEG_DIR=$BASEDIR/ffmpeg X264_DIR=$BASEDIR/x264 make
+GRAPES=$THIRDPARTYLIBS/GRAPES FFMPEG_DIR=$THIRDPARTYLIBS/ffmpeg X264_DIR=$THIRDPARTYLIBS/x264 make clean
+GRAPES=$THIRDPARTYLIBS/GRAPES FFMPEG_DIR=$THIRDPARTYLIBS/ffmpeg X264_DIR=$THIRDPARTYLIBS/x264 make || { echo "Error cimpiling the Streamer" && exit 1; }
 cd ..
 
 #get test scripts
@@ -40,5 +47,5 @@ git clone http://www.disi.unitn.it/~kiraly/SharedGits/Streamers-test.git
 #run a test
 mkdir test
 cd test
-wget http://halo.disi.unitn.it/~cskiraly/video/test.ts
-$BASEDIR/Streamers-test/test.sh -e $BASEDIR/Streamers/streamer-grapes -N 0 -X 0 -v test.ts -o "$BASEDIR/ffmpeg/ffplay -" -O 1
+wget http://halo.disi.unitn.it/~cskiraly/video/test.ts -N
+$BASEDIR/Streamers-test/test.sh -e $BASEDIR/Streamers/streamer-grapes -N 0 -X 0 -v test.ts -o "$THIRDPARTYLIBS/ffmpeg/ffplay -" -O 1 &
