4133e2ed4672017963895b6f0edaec3a3373d461
Csaba Kiraly <kiraly@disi.unitn.it>
2011-05-31 19:05:37 +0200

    change run_player.sh to work directly from the PeerStreamer folder

--- scripts/run_player.sh
+++ scripts/run_player.sh
@@ -6,14 +6,7 @@ fi
 wget --no-cache http://peerstreamer.org/~napawine/release/channels.conf
 if [ ! -f channels.conf ]; then
   echo "File not found on the server, please contact peerstreamer.org"
-else
-  echo "done. Enjoy!"  
-  DIR="PeerStreamer-`git describe --always --dirty || git describe --always`"
-  cp channels.conf ../$DIR
-  cd ..
-  if  [ ! -d $DIR ]; then
-    echo "check git diff and run make"
-  else
-  cd $DIR && ./chunker_player 1>player.log 2>player.err &
-  fi 
+  exit;
 fi
+
+./chunker_player $@
