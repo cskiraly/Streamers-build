f83f187802faccf116fd4d5cdc4566824962d14a
Csaba Kiraly <kiraly@disi.unitn.it>
2011-06-14 14:59:36 +0200

    player.sh: handle better errors in channels.conf download

--- scripts/player.sh
+++ scripts/player.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo -en "Downloading channels list..."
-if [ -f channels.conf ]; then
-  rm -f channels.conf
-fi
-wget --no-cache http://peerstreamer.org/~napawine/release/channels.conf || curl -L -O http://peerstreamer.org/~napawine/release/channels.conf
+wget -N --no-cache http://peerstreamer.org/~napawine/release/channels.conf || curl -f -L -O http://peerstreamer.org/~napawine/release/channels.conf
 if [ ! -f channels.conf ]; then
   echo "File not found on the server, please contact peerstreamer.org"
   exit;
