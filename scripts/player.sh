714526df2156501aed3368232560db5181dfde24
Csaba Kiraly <kiraly@disi.unitn.it>
2011-08-09 11:25:39 +0200

    better kill trap in wrapper script

--- scripts/player.sh
+++ scripts/player.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Kill everything we've stared or on exit (with trap).
-trap "ps -o pid= --ppid $$ | xargs kill 2>/dev/null" 0
+function bashkilltrap()
+{
+  kill -15 -$$
+  ps -o pid= --ppid $$ xargs kill 2>/dev/null
+}
+trap bashkilltrap 0
 
 echo -en "Downloading channels list..."
 wget -N --no-cache http://peerstreamer.org/~napawine/release/channels.conf || curl -f -L -O http://peerstreamer.org/~napawine/release/channels.conf
