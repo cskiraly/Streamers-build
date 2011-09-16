db0a33eebff9326e8da870711ecbaecfb4fb0449
Csaba Kiraly <kiraly@disi.unitn.it>
2011-09-16 18:23:41 +0200

    player: improve handling of channel list file

--- scripts/player.sh
+++ scripts/player.sh
@@ -8,11 +8,6 @@ function bashkilltrap()
 }
 trap bashkilltrap 0
 
-echo -en "Downloading channels list..."
-wget -N --no-cache http://peerstreamer.org/~napawine/release/channels.conf || curl -f -L -O http://peerstreamer.org/~napawine/release/channels.conf
-if [ ! -f channels.conf ]; then
-  echo "File not found on the server, please contact peerstreamer.org"
-  exit;
-fi
+mkdir -p ~/.peerstreamer
 
 ./chunker_player $@
